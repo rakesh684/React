ac98254a0b0baac809f04873453478cbc06e2d81
Rakesh kumar <dr.rakesh684@gmail.com>
2022-04-19 19:07:03 +0530

    learning first step of react

--- index.js
+++ index.js
@@ -17,3 +17,8 @@ ReactDOM.render(
     </div>,
     document.getElementById("root")
 );
+
+const h1=document.createElement("h1");
+h1.textContent="This is rakesh here , May I help you ?";
+h1.className="body";
+document.getElementById("root").append(h1);
